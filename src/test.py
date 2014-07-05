81830c0446c4748ae2c6c3abbc1bad42cdd58ecb
ison-hp <ison@hp.com>
2014-07-05 20:59:15 +0800

    repire something

--- src/test.py
+++ src/test.py
@@ -1,1 +1,5 @@
-print 9876543210
+def baseN(num,b):
+   return ((num == 0) and  "0" ) or ( baseN(num // b, b).lstrip("0") + "0123456789abcdefghijklmnopqrstuvwxyz"[num % b])
+
+print 9876543210/-7
+print 9876543210%-7
